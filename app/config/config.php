5a7bd7fda31d56a4ec29e28ea1e77e6dc3031eb0
mycodelobster <mycodelobster@outlook.com>
2014-02-09 13:21:47 +0700

    edit

--- app/config/config.php
+++ app/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '4szQkXlsGCb2w41pBpyjWmkuQ08zAij6';
 
 /*
 |--------------------------------------------------------------------------
